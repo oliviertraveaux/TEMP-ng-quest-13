6951c60ee72fbcc6185237a363a1846979644eb2
Olivier Traveaux <olivier.traveaux@gmail.com>
2023-09-28 11:05:15 +0200

    lighten form html, isFieldValid and getErrorMessage

--- src/app/pages/create-pokemon-page/create-pokemon-page.component.ts
+++ src/app/pages/create-pokemon-page/create-pokemon-page.component.ts
@@ -1,71 +1,101 @@
 import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import {
-  AbstractControl,
-  FormBuilder,
-  ValidationErrors,
-  ValidatorFn,
-  Validators,
+    AbstractControl,
+    FormBuilder,
+    FormControl,
+    ValidationErrors,
+    ValidatorFn,
+    Validators,
 } from '@angular/forms';
 import { Pokemon } from '../../shared/models/pokemon.model';
 import { PokedexService } from '../../shared/services/pokedex.service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
-  selector: 'app-create-pokemon-page',
-  templateUrl: './create-pokemon-page.component.html',
-  styleUrls: ['./create-pokemon-page.component.scss'],
+    selector: 'app-create-pokemon-page',
+    templateUrl: './create-pokemon-page.component.html',
+    styleUrls: ['./create-pokemon-page.component.scss'],
 })
 export class CreatePokemonPageComponent implements OnInit {
-  private _fb = inject(FormBuilder);
-  private _pokedexService = inject(PokedexService);
-  destroyRef = inject(DestroyRef); // Injecter la référence DestroyeRef pour pouvoir unsubscribe. Evite le ngOnDestroy()
-  public pokemons!: Pokemon[];
-  isNameAvailable(): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
-      const value = control.value;
-      const isAvailable = this.pokemons?.some(
-        pokemon => pokemon.name === value
-      );
+    private _fb = inject(FormBuilder);
+    private _pokedexService = inject(PokedexService);
+    destroyRef = inject(DestroyRef); // Injecter la référence DestroyeRef pour pouvoir unsubscribe. Evite le ngOnDestroy()
+    public pokemons!: Pokemon[];
+    isNameAvailable(): ValidatorFn {
+        return (control: AbstractControl): ValidationErrors | null => {
+            const value = control.value;
+            const isAvailable = this.pokemons?.some(
+                pokemon => pokemon.name === value
+            );
 
-      if (!isAvailable) {
-        return null;
-      } else {
-        return {
-          validValidator: value,
+            if (!isAvailable) {
+                return null;
+            } else {
+                return {
+                    validValidator: value,
+                };
+            }
         };
-      }
-    };
-  }
-
-  createPokemonForm = this._fb.group({
-    name: [
-      '',
-      [Validators.required, Validators.minLength(2), this.isNameAvailable()],
-    ],
-    description: ['', [Validators.required, Validators.minLength(2)]],
-    imageUrl: [
-      '',
-      [
-        Validators.required,
-        Validators.pattern(
-          '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?'
-        ),
-      ],
-    ],
-  });
-
-  ngOnInit() {
-    this._pokedexService.pokemons$
-      .pipe(takeUntilDestroyed(this.destroyRef)) // Permet de unsubscribe lorsque le composant est détruit sans passer par ngOndestroy
-      .subscribe((pokemons: Pokemon[]) => {
-        this.pokemons = pokemons;
-      });
-  }
-
-  onSubmit(): void {
-    if (this.createPokemonForm.valid) {
-      this._pokedexService.addPokemon(this.createPokemonForm.value as Pokemon);
-      this.createPokemonForm.reset();
     }
-  }
+
+    createPokemonForm = this._fb.group({
+        name: [
+            '',
+            [
+                Validators.required,
+                Validators.minLength(2),
+                this.isNameAvailable(),
+            ],
+        ],
+        description: ['', [Validators.required, Validators.minLength(2)]],
+        imageUrl: [
+            '',
+            [
+                Validators.required,
+                Validators.pattern(
+                    '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?'
+                ),
+            ],
+        ],
+    });
+
+    isFieldInvalid(fieldName: string): boolean | undefined {
+        const control = this.createPokemonForm.get(fieldName);
+        return control?.invalid && (control.dirty || control.touched);
+    }
+
+    getErrorMessage(name: string, validator: string) {
+        const control = this.createPokemonForm.get(name);
+
+        if (control && control.hasError(validator)) {
+            switch (validator) {
+                case 'minlength':
+                    return 'Le texte doit faire au moins 2 caractères';
+                case 'pattern':
+                    return 'Cela ne ressemble pas à une URL';
+                case 'validValidator':
+                    return 'Le nom est pris';
+                case 'required':
+                    return 'Champ requis';
+            }
+        }
+        return null;
+    }
+
+    ngOnInit() {
+        this._pokedexService.pokemons$
+            .pipe(takeUntilDestroyed(this.destroyRef)) // Permet de unsubscribe lorsque le composant est détruit sans passer par ngOndestroy
+            .subscribe((pokemons: Pokemon[]) => {
+                this.pokemons = pokemons;
+            });
+    }
+
+    onSubmit(): void {
+        if (this.createPokemonForm.valid) {
+            this._pokedexService.addPokemon(
+                this.createPokemonForm.value as Pokemon
+            );
+            this.createPokemonForm.reset();
+        }
+    }
 }
